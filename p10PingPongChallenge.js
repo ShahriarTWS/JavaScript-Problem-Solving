ce9737da9d2ea51cd7bf5d3e7609f2e966ab3289
Shahriar Nazim Joy <address.shahriar@gmail.com>
2025-08-18 13:35:35 +0600

    solve Problem 10: PingPong Challenge

---
+++ p10PingPongChallenge.js
@@ -0,0 +1,20 @@
+const pingPong = (n) => {
+    let result = [];
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("PingPong");
+        } else if (i % 3 === 0) {
+            result.push("Ping");
+        } else if (i % 5 === 0) {
+            result.push("Pong");
+        } else {
+            result.push(i);
+        }
+    }
+
+    console.log(result.join(", "));
+}
+
+const num = 20;
+pingPong(num);
